35575d9d038242f4d6f4f12ba60ced6c4c940681
Molorn <judekroeze@gmail.com>
2026-02-09 15:34:49 -0600

    Added Pots

---
+++ Assets/Custom/Scripts/Breakable.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class Breakable : MonoBehaviour
+{
+
+    [SerializeField] GameObject fullPot;
+    [SerializeField] GameObject brokenPot;
+
+    BoxCollider bc;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        fullPot.SetActive(true);
+        brokenPot.SetActive(false);
+
+        bc = GetComponent<BoxCollider>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Break();
+        }
+    }
+
+    private void Break()
+    {
+        fullPot.SetActive(false);
+        brokenPot.SetActive(true);
+    }
+}
