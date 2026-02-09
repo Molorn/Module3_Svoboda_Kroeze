d621dbdc300b5bdc5ef64e5f2fb2047d74a8dd10
Molorn <judekroeze@gmail.com>
2026-02-09 17:01:47 -0600

    Finished pots, began enemy AI

---
+++ Assets/Custom/Scripts/Rotator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Rotator : MonoBehaviour
+{
+
+    float timeVar = 0.5f;
+    bool floatUp = false;
+    public bool isGrabbed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 40, 0) * Time.deltaTime);
+
+        timeVar += Time.deltaTime;
+        if (timeVar >= 1f)
+        {
+            floatUp = !floatUp;
+            timeVar = 0f;
+        }
+
+        if (floatUp)
+        {
+            transform.Translate(Vector3.up * 1.0f * Time.deltaTime);
+        }
+        if (!floatUp)
+        {
+            transform.Translate(Vector3.down * 1.0f * Time.deltaTime);
+        }
+    }
+}
